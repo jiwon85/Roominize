f9024921c52962c2daa4e61ac3e1d5735bf2b506
jiwon85 <jiwon.min85@gmail.com>
2013-11-17 17:39:10 -0600

    deleted skeleton_markers

--- skeleton_markers/build/CMakeFiles/listener.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../msg_gen"
-  "../src/skeleton_markers/msg"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- skeleton_markers/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/fri/ros/rosbuild_ws/skeleton_markers/build/devel/setup.sh"
